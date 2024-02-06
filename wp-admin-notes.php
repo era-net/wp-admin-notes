09664e79b3502e95aff991e32300ed7a3aa627e2
era-net <erwinkevinkast@gmail.com>
2024-02-06 05:59:58 +0100

    initial commit

---
+++ wp-admin-notes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WPAdmin Notes
+ * Plugin URI: #
+ * Description: A note block for your admin panel
+ * Version: 1.0.0
+ * Text Domain: wpn-notes
+ * Domain Path: /languages
+ * Author: Ervinator
+ * Author URI: https://era-kast.ch
+ */
